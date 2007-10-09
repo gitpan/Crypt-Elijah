b7619b1b4aaef1912ad640dba84b9be39ad7a241
Michael W. Bombardieri <bombardierix@gmail.com>
2007-10-09 17:37:08 +0000

    Import of BOMB/Crypt-Elijah-0.07 from CPAN.
    gitpan-cpan-distribution: Crypt-Elijah
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         BOMB/Crypt-Elijah-0.07.tar.gz
    gitpan-cpan-author:       BOMB
    gitpan-cpan-maturity:     released

--- lib/Crypt/Elijah.pm
+++ lib/Crypt/Elijah.pm
@@ -1,3 +1,134 @@
+package Elijah;
+
+use 5.006;
+use bytes;
+use Carp;
+
+our $VERSION = '0.07';
+
+sub _encrypt {
+    if (   !defined( $_[0] )
+        || !defined( $_[1] )
+        || ref( $_[0] )
+        || !ref( $_[1] ) )
+    {
+        croak('Argument invalid');
+    }
+    my $len = length( $_[0] );
+    my @t   = unpack( 'C' . $len, $_[0] );
+    my $s   = $_[1];
+    my $pc  = $$s[359];
+    my $i;
+    for ( $i = 0 ; $i < $len ; $i++ ) {
+        $t[$i] = ( $t[$i] + $pc ) % 256;
+        $t[$i] ^= $$s[ $i % 360 ];
+        $pc = ( $t[$i] + $i ) % 256;
+    }
+    $_[0] = pack( 'C' . $len, @t );
+}
+
+sub _decrypt {
+    if (   !defined( $_[0] )
+        || !defined( $_[1] )
+        || ref( $_[0] )
+        || !ref( $_[1] ) )
+    {
+        croak('Argument invalid');
+    }
+    my $len = length( $_[0] );
+    my @t   = unpack( 'C' . $len, $_[0] );
+    my $s   = $_[1];
+    my $pc  = $$s[359];
+    my ( $a, $i );
+    for ( $i = 0 ; $i < $len ; $i++ ) {
+        $a = $t[$i];
+        $t[$i] ^= $$s[ $i % 360 ];
+        $t[$i] = ( $t[$i] - $pc ) % 256;
+        $pc = ( $a + $i ) % 256;
+    }
+    $_[0] = pack( 'C' . $len, @t );
+}
+
+sub encrypt {
+    if (   !defined( $_[0] )
+        || !defined( $_[1] )
+        || ref( $_[0] )
+        || !ref( $_[1] ) )
+    {
+        croak('Argument invalid');
+    }
+    my @x = ( 66, 70, 241, 196 );
+    $x[0] ^= int( rand(256) );
+    $x[1] ^= int( rand(256) );
+    $x[2] ^= int( rand(256) );
+    $x[3] ^= int( rand(256) );
+    $_[0] = pack( 'C4', @x ) . $_[0];
+    my $len = length( $_[0] );
+    my @t   = unpack( 'C' . $len, $_[0] );
+    my $s   = $_[1];
+    my $pc  = $$s[359];
+    my $i;
+
+    for ( $i = 0 ; $i < $len ; $i++ ) {
+        $t[$i] = ( $t[$i] + $pc ) % 256;
+        $t[$i] ^= $$s[ $i % 360 ];
+        $pc = ( $t[$i] + $i ) % 256;
+    }
+    $_[0] = pack( 'C' . $len, @t );
+}
+
+sub decrypt {
+    if (   !defined( $_[0] )
+        || !defined( $_[1] )
+        || ref( $_[0] )
+        || !ref( $_[1] ) )
+    {
+        croak('Argument invalid');
+    }
+    my $len = length( $_[0] );
+    my @t   = unpack( 'C' . $len, $_[0] );
+    my $s   = $_[1];
+    my $pc  = $$s[359];
+    my ( $a, $i );
+    for ( $i = 0 ; $i < $len ; $i++ ) {
+        $a = $t[$i];
+        $t[$i] ^= $$s[ $i % 360 ];
+        $t[$i] = ( $t[$i] - $pc ) % 256;
+        $pc = ( $a + $i ) % 256;
+    }
+    $_[0] = pack( 'C' . $len, @t );
+    $_[0] = substr( $_[0], 4 );
+}
+
+sub set_key {
+    if ( !defined( $_[0] ) || ref( $_[0] ) || ( length( $_[0] ) < 12 ) ) {
+        croak('Argument invalid');
+    }
+    my @k      = ();
+    my @s      = ();
+    my $keylen = length( $_[0] );
+    my ( $a, $b, $c, $d, $e, $pc, $i );
+    if ( $keylen < 16 ) {
+        $a    = 16 - $keylen;
+        $b    = "\0" x $a;
+        $_[0] = $b . $_[0];
+    }
+    @k = unpack( 'C16', $_[0] );
+    $pc = 21;
+    for ( $i = 360 ; $i > 0 ; $i-- ) {
+        $a  = $i % 256;
+        $b  = $a % 16;
+        $c  = ( $b + 1 ) * ( $b + 1 );
+        $d  = ( $c + $a ) % 256;
+        $e  = ( $d + $k[$b] + $pc ) % 256;
+        $pc = $e;
+        push( @s, $e );
+    }
+    return \@s;
+}
+
+1;
+
 =head1 NAME
 
 Crypt::Elijah - cipher module 
@@ -29,174 +160,28 @@ The first argument is a string containing your data.
 The second argument is a reference returned by set_key().
 Salt is added to your data; ciphertext will always be larger than the corresponding plaintext.
 
-=head1 NOTES
+=head1 BUGS
 
-This module is experimental software.
-Please do not rely on the security of this software.
+It is a good idea to remove redundancy from your data prior to encryption (e.g. using compression); this module has no built-in mechanism for achieving this.
+Redundancy in your data may allow information to be discovered from the ciphertext.
 
-This module is not intended for bulk encryption.
-It is more suitable for processing passwords.  
+This module is not intended for bulk encryption.  
 If you pass a large amount of data to encrypt() or decrypt(), it can take a really long time.
 
-It is a good idea to remove redundancy from your data prior to encryption.
-This may be achieved using compression.
-Redundancy in your data may allow an attacker to discover information from the ciphertext.
+This module is experimental software -- use at your own risk.
 
 =head1 AUTHOR
 
-Copyright (c) 2007 Michael W. Bombardieri
+Michael W. Bombardieri <bombardierix@gmail.com>
 
-This software is released under the same license as Perl itself. 
-See the Perl README for details.
+=head1 COPYRIGHT
+
+Copyright 2007 Michael W. Bombardieri.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 DEDICATION
 
 This software is dedicated to Elijah DePiazza.
 
 =cut
-
-
-package Elijah;
-
-use 5.006;
-use strict;
-use warnings;
-use bytes;
-use Carp;
-
-our $VERSION = '0.06';
-
-
-sub _check_key_args { # set_key($key)
-	if (!defined($_[0])) {
-		croak("Argument undefined");
-	} elsif (ref($_[0])) {
-		croak("Argument shouldn't be a reference");
-	} elsif (length($_[0]) < 12) { # min. 96 bit
-		croak("Key is too short");
-	} elsif (length($_[0]) > 16) { # max. 128 bit
-		carp("Warning: truncating key to 16 bytes");
-		$_[0] = substr($_[0], 0, 16);
-	}	
-}
- 
-sub _check_enc_args { # encrypt($text, $keyref)
-	if (!defined($_[0]) || !defined($_[1])) {
-		croak("Argument undefined");
-	} elsif (ref($_[0])) {
-		croak("Text argument shouldn't be a reference");
-	} elsif (!ref($_[1])) {
-		croak("Key argument isn't a reference");
-	}
-}
-
-sub _salt { # 4 bytes
-	my @x = (66, 70, 241, 196);
-	my $padder;
-
-	$x[0] ^= int(rand(256)); $x[1] ^= int(rand(256));
-	$x[2] ^= int(rand(256)); $x[3] ^= int(rand(256));
-	$padder = pack("C4", @x);
-	return $padder;
-}
-
-sub _enc { # encrypt some text
-	my $len = length($_[0]); # text length
-	my @t = unpack("C$len", $_[0]); # unpacked text
-	my $s = $_[1]; # reference to keystream array
-	my $pc = $$s[359];
-	my $i;
-
-	for ($i = 0; $i < $len; $i++) {
-		$t[$i] = ($t[$i] + $pc) % 256;
-		$t[$i] ^= $$s[$i % 360];
-		$pc = ($t[$i] + $i) % 256;
-	}
-	$_[0] = pack("C$len", @t);
-}
-
-sub _encp { # wrapper for probabilistic encryption
-	$_[0] = _salt() . $_[0]; # grow the plaintext	
-	_enc(@_);
-}
-
-sub _dec { # decrypt some text
-	my $len = length($_[0]); # text length
-	my @t = unpack("C$len", $_[0]); # unpacked text
-	my $s = $_[1]; # reference to keystream array
-	my $pc = $$s[359];
-	my ($a, $i); # temporary
-
-	for ($i = 0; $i < $len; $i++) {
-		$a = $t[$i];
-		$t[$i] ^= $$s[$i % 360];
-		$t[$i] = ($t[$i] - $pc) % 256;
-		$pc = ($a + $i) % 256;
-	}
-	$_[0] = pack("C$len", @t);
-}
-
-sub _decp { # wrapper for probabilistic encryption
-	_dec(@_);
-	$_[0] = substr($_[0], 4);
-}
-
-# enable probabilistic encryption by default
-my $_encrypt = \&_encp;
-my $_decrypt = \&_decp;
-
-sub encrypt {
-	_check_enc_args(@_);
-	&$_encrypt(@_);
-}
-
-sub decrypt {
-	_check_enc_args(@_);
-	&$_decrypt(@_);
-}
-
-sub switch { # enable/disable probabilistic encryption
-	if (\&_enc == $_encrypt) {
-		$_encrypt = \&_encp;
-		$_decrypt = \&_decp;
-	} else {
-		$_encrypt = \&_enc;
-		$_decrypt = \&_dec;
-	}
-}
-
-sub probable { # return true if probabilistic encryption is enabled
-	return ($_encrypt == \&_encp) ? 1 : 0;
-}
-
-sub set_key { # allocate keystream and return reference
-	_check_key_args(@_);
-
-	my @k = (); # unpacked user key
-	my @s = (); # keystream
-	my ($a, $b, $c, $d, $e, $pc, $i); # temporary
-	my $key_len = length($_[0]);
-
-	# pad a short key
-	if ($key_len < 16) {
-		$a = 16 - $key_len;
-		$b = "\0" x $a;
-		$_[0] = $b . $_[0];
-	}
-
-	# prepare keystream
-	@k = unpack("C16", $_[0]);
-	$pc = 21;
-	for ($i = 360; $i > 0; $i--) {
-		$a = $i % 256; 
-		$b = $a % 16;
-		$c = ($b + 1) * ($b + 1);
-		$d = ($c + $a) % 256;
-		$e = ($d + $k[$b] + $pc) % 256;
-		$pc = $e;
-		push(@s, $e);
-	}
-	return \@s;
-}
-
-1;
---
+++ t/test.t
@@ -0,0 +1,292 @@
+use strict;
+use Test;
+
+BEGIN {
+	plan tests => 2;
+}
+
+use Crypt::Elijah;
+
+print("# Using Crypt::Elijah version " . $Elijah::VERSION . "\n");
+
+sub test_api {
+	my $key;
+	my $keyref;
+	my $text;
+	my $code;
+	my $tmp;
+	my $plaintext;
+	my $ciphertext;
+	my $newciphertext;
+
+	print("# Elijah::set_key()\n");
+	$code = '$keyref = Elijah::set_key($key); 1;';
+
+	print("# Testing normal key input (max. length)... ");
+	undef($keyref);
+	$key = '0123456789abcdef';
+	if (eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing normal key input (min. length)... ");
+	undef($keyref);
+	$key = '0123456789ab';
+	if (eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid key input (too short)... ");
+	undef($keyref);
+	$key = '0123456789a';
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid key arg (undefined)... ");
+	undef($keyref);
+	undef($key);
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid key arg (reference)... ");
+	undef($keyref);
+	$key = \$text;
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Checking whether set_key() returns a reference... ");
+	undef($keyref);
+	$key = '0123456789abcdef';
+	if (eval($code) && ref($keyref)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Elijah::encrypt()\n");
+	$code = 'Elijah::encrypt($text, $keyref); 1;';
+
+	print("# Testing normal encryption... ");
+	undef($text);
+	$text = 'Jamie works with two hammers';
+	$plaintext = $text;
+	if (eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}	
+	$ciphertext = $text;
+
+	print("# Testing invalid text arg (undefined)... ");
+	undef($text);
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid text arg (reference)... ");
+	undef($text);
+	$text = \$tmp;
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid keyref arg (undefined)... ");
+	$tmp = $keyref;
+	undef($keyref);
+	undef($text);
+	$text = 'Jamie works with two hammers';
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	print("# Testing invalid keyref arg (not a reference)... ");
+	undef($text);
+	$text = 'Jamie works with two hammers';
+	$keyref = "bla";
+	if (!eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+
+	$keyref = $tmp;
+	$text = $ciphertext;
+
+	print("# Elijah::decrypt()\n");
+	$code = 'Elijah::decrypt($text, $keyref); 1;';
+
+	print("# Testing normal decryption... ");
+	if (eval($code) && ($text eq $plaintext)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}	
+
+
+	$text = $plaintext;
+
+	print("# Elijah::_encrypt()\n");
+	$code = 'Elijah::_encrypt($text, $keyref); 1;';
+
+	print("# Testing base encryption... ");
+	if (eval($code)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}	
+	$ciphertext = $text;
+
+	print("# Elijah::_decrypt()\n");
+	$code = 'Elijah::_decrypt($text, $keyref); 1;';
+
+	print("# Testing base decryption... ");
+	if (eval($code) && ($text eq $plaintext)) {
+		print("Done\n");
+	} else {
+		return 0;
+	}
+	
+	return 1;
+}
+
+sub test_cipher_vectors {
+	my @key;
+	my @txt;
+	my @out;
+	my $Key;
+	my $Txt;
+	my $keyref;
+	my $Expected;
+
+	print("# Checking test vectors for Elijah cipher... ");
+
+	@key = (
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+	);
+	@txt = (
+		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA
+	);
+	@out = (
+		0x1A, 0xB7, 0x6B, 0x86, 0x3E, 0x93, 0xAB, 0x16,
+		0x6A, 0x17, 0x83, 0x5E, 0x7E, 0x63, 0x33, 0x0E
+	);
+	$Txt = pack('C16', @txt);
+	$Key = pack('C16', @key);
+	$Expected = pack('C16', @out);
+	$keyref = Elijah::set_key($Key);
+	Elijah::_encrypt($Txt, $keyref);
+	if ($Txt ne $Expected) {
+		return 0;
+	}
+
+	@key = (
+		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA
+	);
+	@txt = (
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+	);
+	@out = (
+		0xDC, 0x12, 0x18, 0x24, 0x42, 0xC4, 0x5E, 0xC6, 
+		0x7C, 0x3A, 0xE0, 0x84, 0x92, 0x2C, 0x16, 0xB6
+	);
+	$Txt = pack('C16', @txt);
+	$Key = pack('C16', @key);
+	$Expected = pack('C16', @out);
+	$keyref = Elijah::set_key($Key);
+	Elijah::_encrypt($Txt, $keyref);
+	if ($Txt ne $Expected) {
+		return 0;
+	}
+
+	@key = (
+		0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,
+		0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,
+	);
+	@txt = (
+		0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11,
+		0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99
+	);
+	@out = (
+		0x30, 0x8E, 0x92, 0x8C, 0x7E, 0x5C, 0xEC, 0x16, 
+		0x48, 0x46, 0x42, 0x3C, 0x86, 0x74, 0x9C, 0x36
+	);
+	$Txt = pack('C16', @txt);
+	$Key = pack('C16', @key);
+	$Expected = pack('C16', @out);
+	$keyref = Elijah::set_key($Key);
+	Elijah::_encrypt($Txt, $keyref);
+	if ($Txt ne $Expected) {
+		return 0;
+	}
+
+	@key = (
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xAD,
+		0xBE, 0xEF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	);
+	@txt = (
+		0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+		0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+	);
+	@out = (
+		0x23, 0xC4, 0x93, 0x48, 0x17, 0xD2, 0xE1, 0x76, 
+		0x81, 0xD4, 0x43, 0xF0, 0x3F, 0xE2, 0x71, 0xAD
+	);
+	$Txt = pack('C16', @txt);
+	$Key = pack('C16', @key);
+	$Expected = pack('C16', @out);
+	$keyref = Elijah::set_key($Key);
+	Elijah::_encrypt($Txt, $keyref);
+	if ($Txt ne $Expected) {
+		return 0;
+	}
+
+	@key = (
+		0xF0, 0x1F, 0xF2, 0x3F, 0xF4, 0x5F, 0xF6, 0x7F,
+		0xF8, 0x9F, 0xFA, 0xBF, 0xFC, 0xDF, 0xFE, 0xFF
+	);
+	@txt = (
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	);
+	@out = (
+		0x03, 0xEF, 0x89, 0xEA, 0x3F, 0xC7, 0x2D, 0x56, 
+		0xEB, 0xE7, 0xA1, 0xFA, 0x57, 0x9F, 0x65, 0x56
+	);
+	$Txt = pack('C16', @txt);
+	$Key = pack('C16', @key);
+	$Expected = pack('C16', @out);
+	$keyref = Elijah::set_key($Key);
+	Elijah::_encrypt($Txt, $keyref);
+	if ($Txt ne $Expected) {
+		return 0;
+	}
+
+	print("Done\n");
+	return 1;
+}
+
+ok(test_api());
+ok(test_cipher_vectors());
